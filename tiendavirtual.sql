7dad01459055f3f1c978b8f0ce2191d2d02ace0f
danielherrera1999 <44683454+danielherrera1999@users.noreply.github.com>
2021-03-03 17:33:20 -0500

    bd actualizada
    aqui actualice la base de datos

--- tiendavirtual.sql
+++ tiendavirtual.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2021 a las 01:56:42
+-- Tiempo de generación: 03-03-2021 a las 23:32:10
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.2.34
 
@@ -37,8 +37,143 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id_categories`, `categories`) VALUES
-(1, 'joggers'),
-(2, 'camibuso');
+(1, 'jeanss');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `city`
+--
+
+CREATE TABLE `city` (
+  `id_city` int(11) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `id_department` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `city`
+--
+
+INSERT INTO `city` (`id_city`, `city`, `id_department`) VALUES
+(1, 'neiva', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contact`
+--
+
+CREATE TABLE `contact` (
+  `id_contact` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `number` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `contact`
+--
+
+INSERT INTO `contact` (`id_contact`, `type`, `number`, `id_user`) VALUES
+(1, 'celular', 2147483647, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `country`
+--
+
+CREATE TABLE `country` (
+  `id_country` int(11) NOT NULL,
+  `country` varchar(20) NOT NULL,
+  `id_postal_code` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `country`
+--
+
+INSERT INTO `country` (`id_country`, `country`, `id_postal_code`) VALUES
+(1, 'colombia', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `department`
+--
+
+CREATE TABLE `department` (
+  `id_department` int(11) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  `id_country` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `department`
+--
+
+INSERT INTO `department` (`id_department`, `department`, `id_country`) VALUES
+(1, 'huila', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localization`
+--
+
+CREATE TABLE `localization` (
+  `id_localization` int(11) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `country` int(11) NOT NULL,
+  `department` int(11) NOT NULL,
+  `city` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `localization`
+--
+
+INSERT INTO `localization` (`id_localization`, `address`, `country`, `department`, `city`, `id_user`) VALUES
+(1, 'cra 5a #54-52', 1, 1, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `payment_method`
+--
+
+CREATE TABLE `payment_method` (
+  `id_method` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `payment_method`
+--
+
+INSERT INTO `payment_method` (`id_method`, `type`) VALUES
+(1, 'contraentrega');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postal_code`
+--
+
+CREATE TABLE `postal_code` (
+  `id_code` int(11) NOT NULL,
+  `code` varchar(20) NOT NULL,
+  `flags` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `postal_code`
+--
+
+INSERT INTO `postal_code` (`id_code`, `code`, `flags`) VALUES
+(1, '+57', 'colombia');
 
 -- --------------------------------------------------------
 
@@ -60,7 +195,68 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id_product`, `product`, `categories_product`, `images_product`, `price_product`, `stock_product`) VALUES
-(1, 'joggers MHC ', 1, 0, 25000, 10);
+(1, 'jean mbd', 1, 0, 25000, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id_rol` int(11) NOT NULL,
+  `roles` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id_rol`, `roles`) VALUES
+(1, 'cliente'),
+(2, 'administra');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `shoppingcart`
+--
+
+CREATE TABLE `shoppingcart` (
+  `id_cart` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `product` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `state` int(11) NOT NULL,
+  `payment_method` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `shoppingcart`
+--
+
+INSERT INTO `shoppingcart` (`id_cart`, `user`, `product`, `total`, `state`, `payment_method`) VALUES
+(4, 2, 1, 20000, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `state_checkout`
+--
+
+CREATE TABLE `state_checkout` (
+  `id_state` int(11) NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `icon` varchar(20) NOT NULL,
+  `colors` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `state_checkout`
+--
+
+INSERT INTO `state_checkout` (`id_state`, `state`, `icon`, `colors`) VALUES
+(1, 'pendiente', 'alerta ', 'gris');
 
 -- --------------------------------------------------------
 
@@ -73,8 +269,16 @@ CREATE TABLE `users` (
   `name_user` varchar(20) NOT NULL,
   `lastname_user` varchar(20) NOT NULL,
   `email_user` varchar(20) NOT NULL,
-  `password_user` varchar(20) NOT NULL
+  `password_user` varchar(20) NOT NULL,
+  `rol` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id_user`, `name_user`, `lastname_user`, `email_user`, `password_user`, `rol`) VALUES
+(2, 'daniel', 'herrera', 'daniel@gmail.com', '1234', 1);
 
 --
 -- Índices para tablas volcadas
@@ -87,11 +291,90 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id_categories`);
 
 --
+-- Indices de la tabla `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`id_city`),
+  ADD KEY `city_ibfk_1` (`id_department`);
+
+--
+-- Indices de la tabla `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id_contact`),
+  ADD KEY `contact_ibfk_1` (`id_user`);
+
+--
+-- Indices de la tabla `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`id_country`),
+  ADD KEY `id_postal_code` (`id_postal_code`);
+
+--
+-- Indices de la tabla `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id_department`),
+  ADD KEY `department_ibfk_1` (`id_country`);
+
+--
+-- Indices de la tabla `localization`
+--
+ALTER TABLE `localization`
+  ADD PRIMARY KEY (`id_localization`),
+  ADD KEY `country` (`country`),
+  ADD KEY `department` (`department`),
+  ADD KEY `city` (`city`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indices de la tabla `payment_method`
+--
+ALTER TABLE `payment_method`
+  ADD PRIMARY KEY (`id_method`);
+
+--
+-- Indices de la tabla `postal_code`
+--
+ALTER TABLE `postal_code`
+  ADD PRIMARY KEY (`id_code`);
+
+--
 -- Indices de la tabla `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id_product`),
   ADD KEY `categories_product` (`categories_product`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `shoppingcart`
+--
+ALTER TABLE `shoppingcart`
+  ADD PRIMARY KEY (`id_cart`),
+  ADD KEY `user` (`user`),
+  ADD KEY `product` (`product`),
+  ADD KEY `state` (`state`),
+  ADD KEY `payment_method` (`payment_method`);
+
+--
+-- Indices de la tabla `state_checkout`
+--
+ALTER TABLE `state_checkout`
+  ADD PRIMARY KEY (`id_state`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `users_ibfk_1` (`rol`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -101,23 +384,137 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id_categories` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_categories` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `city`
+--
+ALTER TABLE `city`
+  MODIFY `id_city` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id_contact` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `country`
+--
+ALTER TABLE `country`
+  MODIFY `id_country` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `department`
+--
+ALTER TABLE `department`
+  MODIFY `id_department` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `localization`
+--
+ALTER TABLE `localization`
+  MODIFY `id_localization` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `payment_method`
+--
+ALTER TABLE `payment_method`
+  MODIFY `id_method` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `postal_code`
+--
+ALTER TABLE `postal_code`
+  MODIFY `id_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `shoppingcart`
+--
+ALTER TABLE `shoppingcart`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `state_checkout`
+--
+ALTER TABLE `state_checkout`
+  MODIFY `id_state` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `city`
+--
+ALTER TABLE `city`
+  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`id_department`) REFERENCES `department` (`id_department`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `contact`
+--
+ALTER TABLE `contact`
+  ADD CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `country`
+--
+ALTER TABLE `country`
+  ADD CONSTRAINT `country_ibfk_2` FOREIGN KEY (`id_postal_code`) REFERENCES `postal_code` (`id_code`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `department`
+--
+ALTER TABLE `department`
+  ADD CONSTRAINT `department_ibfk_1` FOREIGN KEY (`id_country`) REFERENCES `country` (`id_country`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `localization`
+--
+ALTER TABLE `localization`
+  ADD CONSTRAINT `localization_ibfk_1` FOREIGN KEY (`country`) REFERENCES `country` (`id_country`),
+  ADD CONSTRAINT `localization_ibfk_2` FOREIGN KEY (`department`) REFERENCES `department` (`id_department`),
+  ADD CONSTRAINT `localization_ibfk_3` FOREIGN KEY (`city`) REFERENCES `city` (`id_city`),
+  ADD CONSTRAINT `localization_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categories_product`) REFERENCES `categories` (`id_categories`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `shoppingcart`
+--
+ALTER TABLE `shoppingcart`
+  ADD CONSTRAINT `shoppingcart_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `shoppingcart_ibfk_2` FOREIGN KEY (`product`) REFERENCES `products` (`id_product`),
+  ADD CONSTRAINT `shoppingcart_ibfk_3` FOREIGN KEY (`state`) REFERENCES `state_checkout` (`id_state`),
+  ADD CONSTRAINT `shoppingcart_ibfk_4` FOREIGN KEY (`payment_method`) REFERENCES `payment_method` (`id_method`);
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rol`) REFERENCES `rol` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
